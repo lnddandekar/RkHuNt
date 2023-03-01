7d432613573a34e124943a46e6e2fb6f62e47025
lnddandekar <50483930+lnddandekar@users.noreply.github.com>
2023-03-01 15:56:00 +0000

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,21 @@
+// random value generated
+  var y=Math.floor(Math.random()*10+1);
+var x=document.getElementById("guessField").value;
+if(x==y)
+{
+    alert("CONGRAGULATIONS!!!"+playername+"YOU GUESSED IT RIGHT IN"
+    +guess+"GUESS");
+    guess=1;
+}
+else if(x>y)
+{
+    guess++;
+    alert("OOPS SORRY!!TRY A SMALLER NUMBER");
+}
+else{
+    guess++;
+    alert("OOPS SORRY!!TRY A GREATER NUMBER")
+}
+function playAgain(){
+    y=Math.floor(Mat.random()*10+1);
+}
